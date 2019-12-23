5fc3dc17879297bb26361db5ea78c0e33cfe4f27
Avery <bartnik314@gmail.com>
2019-12-22 20:53:51 -0600

    add transparency shader autostart

--- rc.lua
+++ rc.lua
@@ -202,7 +202,26 @@ run_once({
 	{"unclutter --timeout 5"},
 	{"mpd"},
 	{"light-locker"},
-	{"~/git/compton-shaders/shader.sh ~/git/compton-shaders/compton-fake-transparency-fshader-win.glsl"},
+	{"compton --backend glx --force-win-blend --use-damage --glx-fshader-win '\
+		uniform float opacity;\
+		uniform bool invert_color;\
+		uniform sampler2D tex;\
+		void main() {\
+			vec4 c = texture2D(tex, gl_TexCoord[0].xy);\
+			if (!invert_color) { // Hack to allow compton exceptions\
+				// Change the vec4 to your desired key color\
+				//vec4 vdiff = abs(vec4(0.0039, 0.0039, 0.0039, 1.0) - c); // #010101\
+				vec4 vdiff = abs(vec4(0, 0.0039, 0.0078, 1.0) - c); // #000102\
+				float diff = max(max(max(vdiff.r, vdiff.g), vdiff.b), vdiff.a);\
+				// Change the vec4 to your desired output color\
+				if (diff < 0.001)\
+					c = vec4(0.0, 0.0, 0.0, 0.7); // #000000E3\
+					//c = vec4(0.0, 0.0, 0.0, 0.890196); // #000000E3\
+			}\
+			c *= opacity;\
+			gl_FragColor = c;\
+		}'\
+	 "};
 })
 
 -- append platform run_once
