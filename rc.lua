05e3abbcdbb90c3e184a8b5c34f065dceb36fc61
Jythonscript <bartnik314@gmail.com>
2020-03-21 10:11:43 -0500

    middle click mouse in tasklist to kill

--- rc.lua
+++ rc.lua
@@ -238,56 +238,60 @@ elseif selectedConfig == "aragorn" then
 end
 
 awful.util.taglist_buttons = gears.table.join(
-                    awful.button({ }, 1, function(t) t:view_only() end),
-                    awful.button({ modkey }, 1, function(t)
-                                              if client.focus then
-                                                  client.focus:move_to_tag(t)
-                                              end
-                                          end),
-                    awful.button({ }, 3, awful.tag.viewtoggle),
-                    awful.button({ modkey }, 3, function(t)
-                                              if client.focus then
-                                                  client.focus:toggle_tag(t)
-                                              end
-                                          end),
-                    awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
-                    awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
-                )
-awful.util.tasklist_buttons = gears.table.join(
-                     awful.button({ }, 1, function (c)
-                                              if c == client.focus then
-                                                  c.minimized = true
-                                              else
-                                                  -- Without this, the following
-                                                  -- :isvisible() makes no sense
-                                                  c.minimized = false
-                                                  if not c:isvisible() and c.first_tag then
-                                                      c.first_tag:view_only()
-                                                  end
-                                                  -- This will also un-minimize
-                                                  -- the client, if needed
-                                                  client.focus = c
-                                                  c:raise()
-                                              end
-                                          end),
-                     awful.button({ }, 3, function()
-                         local instance = nil
+awful.button({ }, 1, function(t) t:view_only() end),
+awful.button({ modkey }, 1, function(t)
+	if client.focus then
+		client.focus:move_to_tag(t)
+	end
+end),
+awful.button({ }, 3, awful.tag.viewtoggle),
+awful.button({ modkey }, 3, function(t)
+	if client.focus then
+		client.focus:toggle_tag(t)
+	end
+end),
+awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
+awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
+)
 
-                         return function ()
-                             if instance and instance.wibox.visible then
-                                 instance:hide()
-                                 instance = nil
-                             else
-                                 instance = awful.menu.clients({ theme = { width = 250 } })
-                             end
-                        end
-                     end),
-                     awful.button({ }, 4, function ()
-                                              awful.client.focus.byidx(1)
-                                          end),
-                     awful.button({ }, 5, function ()
-                                              awful.client.focus.byidx(-1)
-                                          end))
+awful.util.tasklist_buttons = gears.table.join(
+awful.button({ }, 1, function (c)
+	if c == client.focus then
+		c.minimized = true
+	else
+		-- Without this, the following
+		-- :isvisible() makes no sense
+		c.minimized = false
+		if not c:isvisible() and c.first_tag then
+			c.first_tag:view_only()
+		end
+		-- This will also un-minimize
+		-- the client, if needed
+		client.focus = c
+		c:raise()
+	end
+end),
+awful.button({ }, 2, function(c)
+	c:kill()
+end),
+awful.button({ }, 3, function()
+	local instance = nil
+
+	return function ()
+		if instance and instance.wibox.visible then
+			instance:hide()
+			instance = nil
+		else
+			instance = awful.menu.clients({ theme = { width = 250 } })
+		end
+	end
+end),
+awful.button({ }, 4, function ()
+	awful.client.focus.byidx(1)
+end),
+awful.button({ }, 5, function ()
+	awful.client.focus.byidx(-1)
+end))
 
 lain.layout.termfair.nmaster           = 3
 lain.layout.termfair.ncol              = 1
