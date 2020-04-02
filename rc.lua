e5a0a11904151f74209ec02446fdbe14a677ab3b
Jythonscript <bartnik314@gmail.com>
2020-04-01 21:43:52 -0500

    expand layout function formatting

--- rc.lua
+++ rc.lua
@@ -773,10 +773,16 @@ globalkeys = gears.table.join(
               {description = "increase the number of columns", group = "layout"}),
     awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1, nil, true)    end,
               {description = "decrease the number of columns", group = "layout"}),
-    awful.key({ modkey,           }, "space", function () awful.layout.inc( 1)                end,
-              {description = "select next", group = "layout"}),
-    awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(-1)                end,
-              {description = "select previous", group = "layout"}),
+    awful.key({ modkey,           }, "space", 
+		function ()
+			awful.layout.inc(1)
+		end,
+              {description = "select next layout", group = "layout"}),
+    awful.key({ modkey, "Shift"   }, "space", 
+		function ()
+			awful.layout.inc(-1)
+		end,
+              {description = "select previous layout", group = "layout"}),
 
     awful.key({ modkey, "Control" }, "n",
               function ()
